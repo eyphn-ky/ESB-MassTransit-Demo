a59232a41341a2ac11024983e977d405d0706011
eyphn-ky <eyuphan.kaya34@gmail.com>
2023-04-09 17:23:57 +0300

    Request&Response Pattern Applied

---
+++ Consumer/Consumers/RequestMessageConsumer.cs
@@ -0,0 +1,22 @@
+﻿using MassTransit;
+using Shared.RequestResponseMessages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Consumer.Consumers
+{
+    public class RequestMessageConsumer : IConsumer<RequestMessage>
+    {
+        public async Task Consume(ConsumeContext<RequestMessage> context)
+        {
+            //.....process
+            Console.WriteLine(context.Message.Text);
+
+            //işlem tamamlandıktan sonra geriye response mahiyetinde bir mesaj dönmek gerekir. Dolayısıyla context.RespondMessage metodunu kullanarak bun sağlarız.
+            await context.RespondAsync<ResponseMessage>(new() { Text=$"{context.Message.MessageNo}. response to request"});
+        }
+    }
+}
--- Publisher/Program.cs
+++ Publisher/Program.cs
@@ -1,22 +1,44 @@
 ﻿using MassTransit;
 using Shared.Messages;
+using Shared.RequestResponseMessages;
+Console.WriteLine("Publisher");
 
 string rabbitMQUri = "amqps://oxystewb:g8ZxUdsUXLX2FY6u0PfNIRftYF7dc2HC@moose.rmq.cloudamqp.com/oxystewb";
 string queueName = "example-queue";
-//bütün işlemler bus sınıfı üzerinden yürütülecektir.
-IBusControl bus =Bus.Factory.CreateUsingRabbitMq(factory =>
+string requestQueue = "request-queue";
+////bütün işlemler bus sınıfı üzerinden yürütülecektir.
+//IBusControl bus =Bus.Factory.CreateUsingRabbitMq(factory =>
+//{
+//    factory.Host(rabbitMQUri);
+//});
+////ilgili kuyruğa mesaj gönderme işlemi
+//ISendEndpoint sendEndpoint=await bus.GetSendEndpoint(new($"{rabbitMQUri}/{queueName}"));
+
+//Console.Write("Gönderilecek Mesaj : ");
+//string message = Console.ReadLine();
+////tek bir kuyruğa mesaj yollanacaksa Send metodu kullanılır.
+//await sendEndpoint.Send<IMessage>(new ExampleMessage()
+//{
+//    Text = message
+//});
+//Console.Read();
+//---------------REQUEST-RESPONSE PATTERN UYGULAMASI---------------------------//
+IBusControl bus = Bus.Factory.CreateUsingRabbitMq(factory =>
 {
     factory.Host(rabbitMQUri);
 });
-//ilgili kuyruğa mesaj gönderme işlemi
-ISendEndpoint sendEndpoint=await bus.GetSendEndpoint(new($"{rabbitMQUri}/{queueName}"));
+//Publisher request yapacak ama aynı zamanda response'u da okuması gerekiyor. Dolayısıyla Host'u start etmek gerekiyor.
+await bus.StartAsync();
+//Request yapılacağı zaman bu şekilde ayar yapılır.
+var request = bus.CreateRequestClient<RequestMessage>(new Uri($"{rabbitMQUri}/{requestQueue}"));
 
-Console.Write("Gönderilecek Mesaj : ");
-string message = Console.ReadLine();
-//tek bir kuyruğa mesaj yollanacaksa Send metodu kullanılır.
-await sendEndpoint.Send<IMessage>(new ExampleMessage()
+int i = 1;
+while (true)
 {
-    Text = message
-});
-Console.Read();
+    await Task.Delay(200);
+    //verilen türde bir response beklediğini bildirip request attık.
+    var response = await request.GetResponse<ResponseMessage>(new() { MessageNo = i,Text=$"{i++}. request" });
+    Console.WriteLine($"Response Received: {response.Message.Text}");
 
+}
+Console.Read();
